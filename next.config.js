4335f1b48c7bffef7e1c35c374bc6bebe009869d
조다니엘(Daniel Cho) <arduinocc04@gmail.com>
2024-10-05 13:17:06 +0900

    remove i18n

--- next.config.js
+++ next.config.js
@@ -2,11 +2,7 @@
 const nextConfig = {
     images: {
         domains: ["t1.daumcdn.net", "images.unsplash.com"],
-    },
-    i18n: {
-        locales: ['en', 'ko'],
-        defaultLocale: 'en',
-    },
+    }
 }
 
 module.exports = nextConfig
