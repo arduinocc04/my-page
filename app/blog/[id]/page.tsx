e55c7ee950df38563f3560505df5ac17de5be516
조다니엘(Daniel Cho) <arduinocc04@gmail.com>
2024-10-31 09:17:55 +0900

    update blog post design

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -1,27 +1,50 @@
-import {getPostInfoBySlug} from '../../bloghome/post';
+import {getPostInfoBySlug, getAllPostInfos} from '../../bloghome/post';
 import {getPostHtml} from '../blog';
+
+import Link from 'next/link'
+
+import "yorha/dist/yorha.css"
+import "../../styles/added-yorha.css"
+import "../../styles/layout.css"
+
+function PostButton({name, link}: {name: string, link: string}) {
+  return (
+      <Link href={`/blog/${link}`}>
+          <div className='postbutton'>
+              &nbsp;&#x25A0;&nbsp;{name}
+          </div>
+      </Link>
+  );
+}
 
 export default function Post({ params }: { params: {id: string}}) {
     const id = params.id;
 
     const postInfos = getPostInfoBySlug(`${id}`, ["title", "date", "tags"]);
     const html = getPostHtml(id);
+
+    const post_thumbnails = getAllPostInfos(['title', 'summary', 'thumbnail_src', 'url', 'date', 'tags']);
   return (
-    <main className="antialiased flex flex-col min-h-screen dark:bg-gray-900">
-        <div className="flex-1">
-            <div className="relative dark:bg-gray-900">
-                <div className="mx-auto max-w-screen-xl">
-                    <div className="px-6 pt-16 lg:pt-28 pb-16 md:pb-24">
-                        <h1 className="pb-6 text-5xl md:text-6xl lg:text-8xl font-headline font-black tracking-snug text-center leading-12 sm:leading-15 md:leading-19 lg:leading-26 text-gray-800 dark:text-gray-100">
-                            <span>{postInfos.title}</span>
-                        </h1>
-                    </div>
+    <main className="">
+        <div className='yorha-flex'>
+            <h1>
+                블로그
+                <span className='subtitle'>
+                -{postInfos.title}
+                </span>
+            </h1>
+            <div className="post-flex">
+              <blockquote>
+                  {post_thumbnails.map((post_thumbnail) => (
+                      <PostButton key={post_thumbnail._id} name={post_thumbnail.title} link={post_thumbnail.url}/>
+                  ))}
+              </blockquote>
+              <div className="post-frame">
+                <div className='post-title'>&nbsp;&#x25A0;&nbsp;{postInfos.title}</div>
+                <div className='post'>
+                  <div dangerouslySetInnerHTML={{ __html: html }} />
                 </div>
-                <div className="mx-auto max-w-screen-xl">
-                    <div className="px-6 py-5 flex items-center space-y-0">
-                        <div dangerouslySetInnerHTML={{ __html: html }} />
-                    </div>
-                </div>
+              </div>
             </div>
         </div>
     </main>
